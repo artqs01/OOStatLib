0b1b9639a5a7f4cc4e6c0960eafb6688ce7521a4
artqs01 <kus.artur78@gmail.com>
2023-01-02 21:23:56 +0100

    shapiro-wilk

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,31 @@
 #include "shapiro_wilk.hpp"
 #include <iostream>
 
+// #include "test.hpp"
+
 int main()
 {
-	sl::single_data<double> data1 = {0.0, 1.0, 2.0, 3.0, 4.0};
+	sl::single_data<double> data1 = {
+		26.5,
+		27.5,
+		27.5,
+		26.9,
+		27.9,
+		27.0,
+		27.7,
+		27.6,
+		27.9,
+		27.4,
+		28.3,
+		27.6,
+		26.3,
+		25.9,
+		27.9
+	};
 	sl::sw::shapiro_wilk<double> sw1(std::make_shared<sl::single_data<double>>(data1), 0.05);
-	sw1();
-	std::cout << sw1.get_significance() << "\n";
-	std::cout << sl::sw::get_coefficient(20, 9);
+	std::cout << sw1();
+	// std::cout << sw1.get_significance() << "\n";
+	// std::cout << sl::sw::get_coefficient(20, 9);
+
+	// B<"dupa"> b;
 }
