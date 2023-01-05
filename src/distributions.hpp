12da006b4a11b56e87b5f83a15353dce8ac4558f
artqs01 <kus.artur78@gmail.com>
2023-01-05 12:57:29 +0100

    implement chi squared and t student's distribution

--- src/distributions.hpp
+++ src/distributions.hpp
@@ -2,17 +2,38 @@
 #define DISTRIBUTIONS_HPP
 
 #include "data.hpp"
+#include <cassert>
+#include <cmath>
+#include <math.h>
 
 namespace sl 
 {
 
-namespace dst
+namespace pdf
 {
 
-template<UnsignedIntegral DegreesOfFreeedom, Ratio ReturnType = double, Interval ArgumentType = double>
-ReturnType chi_sqr(ArgumentType value)
+template<Ratio T = double>
+T chi_sqr(T value, size_t degrees_of_freedom)
 {
-	return ;
+	assert(!std::isnan(value));
+	assert(!std::isinf(value));
+
+	if (value < 0.0 || degrees_of_freedom < 1) {
+		return 0.0;
+	}
+	T k = degrees_of_freedom * 0.5;
+	return std::pow(value, k - 1) * std::exp(value * (T)0.5) / (std::pow((T)2, k) * std::tgamma(k));
+}
+
+template<Ratio T = double>
+T t(T value, size_t degrees_of_freedom)
+{
+	assert(!std::isnan(value));
+	assert(!std::isinf(value));
+
+	T base = (1 + value * value / (T)degrees_of_freedom) /
+		(std::sqrt((T)degrees_of_freedom) * std::beta((T)0.5, (T)degrees_of_freedom * (T)0.5));
+	return std::pow(base, (T)-0.5 * (T)(degrees_of_freedom + 1));
 }
 
 }
