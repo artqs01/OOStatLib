2e7eebe5f43a706121ddb46d6b1ee060ee916028
artqs01 <kus.artur78@gmail.com>
2023-01-06 13:08:14 +0100

    testing variance and mean significance test

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
-#include "shapiro_wilk.hpp"
+#include "data.hpp"
+#include "distributions.hpp"
+#include "single_data_algorhithms.hpp"
 #include <iostream>
+#include <memory>
 
 int main()
 {
@@ -20,10 +23,22 @@ int main()
 		25.9,
 		27.9
 	};
-	sl::shapiro_wilk<double> sw1(std::make_shared<sl::single_container<double>>(data1), 0.05);
-	std::cout << sw1() << "\n\n";
+	auto data_ptr = std::make_shared<sl::single_container<double>>(data1);
+	sl::shapiro_wilk<double> sw1(
+		data_ptr,
+		sl::significance::five_hundredths);
+	sl::mean_significance_test<double> mst(
+		data_ptr,
+		sl::significance::five_hundredths,
+		27.0);
+	sl::variance_significance_test<double> vst(
+		data_ptr,
+		sl::significance::five_hundredths,
+		27.0);
+	std::cout << "sw: " << sw1() << ", mst: " << mst() << ", vst: " << vst() << "\n\n";
 	// std::cout << sw1.get_significance() << "\n";
 	// std::cout << sl::sw::get_coefficient(20, 9);
-
+	// std::cout << sl::pdf_t(1.87866958564078, 14) << "\n\n";
+	// std::cout << sl::pdf_chi_sqr(3.88675680850485, 7) << "\n";
 	// B<"dupa"> b;
 }
