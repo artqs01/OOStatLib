52cca97ee1cb443de666dbe5205f1c76c20790dc
artqs01 <kus.artur78@gmail.com>
2023-01-06 13:06:26 +0100

    repair pdf functions

--- src/distributions.hpp
+++ src/distributions.hpp
@@ -4,6 +4,7 @@
 #include "data.hpp"
 #include <cassert>
 #include <cmath>
+#include <iostream>
 #include <math.h>
 
 namespace sl 
@@ -18,8 +19,8 @@ T pdf_chi_sqr(T value, size_t degrees_of_freedom)
 	if (value < 0.0 || degrees_of_freedom < 1) {
 		return 0.0;
 	}
-	T k = degrees_of_freedom * 0.5;
-	return std::pow(value, k - 1) * std::exp(value * (T)0.5) / (std::pow((T)2, k) * std::tgamma(k));
+	T k = degrees_of_freedom * (T)0.5;
+	return std::pow(value, k - 1) * std::exp(value * (T)-0.5) / (std::pow((T)2, k) * std::tgamma(k));
 }
 
 template<Ratio T = double>
@@ -28,9 +29,8 @@ T pdf_t(T value, size_t degrees_of_freedom)
 	assert(!std::isnan(value));
 	assert(!std::isinf(value));
 
-	T base = (1 + value * value / (T)degrees_of_freedom) /
+	return std::pow(1 + value * value / (T)degrees_of_freedom, (T)-0.5 * (T)(degrees_of_freedom + 1)) /
 		(std::sqrt((T)degrees_of_freedom) * std::beta((T)0.5, (T)degrees_of_freedom * (T)0.5));
-	return std::pow(base, (T)-0.5 * (T)(degrees_of_freedom + 1));
 }
 
 }
