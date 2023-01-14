e70bd58885efaa7399c2ea6b97966228429b9c29
artqs01 <kus.artur78@gmail.com>
2023-01-14 22:48:33 +0100

    refactoring of significance and minor changes

--- src/data.hpp
+++ src/data.hpp
@@ -1,11 +1,8 @@
 #ifndef DATA_HPP
 #define DATA_HPP
 
-#include <cstddef>
-#include <cassert>
 #include <type_traits>
 #include <vector>
-#include <memory>
 #include <concepts>
 
 namespace sl 
@@ -26,12 +23,6 @@ concept Ratio = std::is_floating_point_v<T>;
 template<typename T>
 concept UnsignedIntegral = std::is_integral_v<T> && std::is_unsigned_v<T>;
 
-template<class T>
-concept Test = requires(T test)
-{
-	{test()} -> std::same_as<bool>;
-};
-
 enum class significance
 {
 	one_hundredth = 1,
@@ -40,70 +31,14 @@ enum class significance
 	one_tenth = 10
 };
 
-template<Ratio T = double>
-class significance_component
-{
-	public:
-		significance_component(significance alfa) : alfa_(alfa) {
-		}
-		void set_significance(significance alfa) {
-			alfa_ = alfa;
-		}
-		T get_significance() {
-			return (T)alfa_ * (T)0.01;
-		}
-		
-	protected:
-		significance alfa_ = significance::five_hundredths;
-};
-
-template<class TestType, typename AlfaType>
-concept Significance = Ratio<AlfaType> && requires(TestType test)
-{
-	std::is_base_of_v<significance_component<AlfaType>, decltype(test)>;
-};
-
-template<typename T>
-class data_container
-{
-	public:
-		data_container(std::shared_ptr<T> data) : data_(data) {}
-		void set_data(std::shared_ptr<T> data) {
-			data_ = data;
-		}
-		std::shared_ptr<T> get_data() {
-			return data_;
-		}
-	protected:
-		std::shared_ptr<T> data_;
-};
-
 template<typename T>
 using single_container = typename std::vector<T>;
 
 template<typename T>
-using paired_data = typename std::vector<std::pair<T, T>>;
+using paired_container = typename std::vector<std::pair<T, T>>;
 
 template<typename T>
 using multi_sample_data = typename std::vector<std::vector<T>>;
-
-template<typename TestType, typename DataType>
-concept SingleDataContainer = requires(TestType test)
-{
-	std::is_base_of_v<data_container<single_container<DataType>>, decltype(test)>;
-};
-
-template<typename TestType, typename DataType>
-concept PairedDataContainer = requires(TestType test)
-{
-	std::is_base_of_v<data_container<paired_data<DataType>>, decltype(test)>;
-};
-
-template<typename TestType, typename DataType>
-concept MultiSampleContainer = requires(TestType test)
-{
-	std::is_base_of_v<data_container<multi_sample_data<DataType>>, decltype(test)>;
-};
 
 }
 
---
+++ src/logic.hpp
@@ -0,0 +1,97 @@
+#ifndef LOGIC_HPP
+#define LOGIC_HPP
+
+#include "data.hpp"
+#include <memory>
+#include <type_traits>
+
+namespace sl {
+
+template<Ratio AlfaType = double>
+class significance_logic
+{
+	public:
+		significance_logic(significance alfa) : alfa_(alfa) {}
+		template<Ratio T>
+		static constexpr inline T to_floating_point(significance alfa) {
+			return (AlfaType)alfa * (AlfaType)0.01;
+		}
+		void set_significance(significance alfa) {
+			alfa_ = alfa;
+		}
+		AlfaType get_significance() {
+			return to_floating_point<AlfaType>(alfa_);
+		}
+	protected:
+		significance alfa_ = significance::five_hundredths;
+};
+
+template<class TestType, typename AlfaType>
+concept Significance = Ratio<AlfaType> && requires(TestType test)
+{
+	std::is_base_of_v<significance_logic<AlfaType>, decltype(test)>;
+};
+
+template<typename DataType>
+class data_logic
+{
+	public:
+		data_logic(std::shared_ptr<DataType> data) : data_(data) {}
+		void set_data(std::shared_ptr<DataType> data) {
+			data_ = data;
+		}
+		std::shared_ptr<DataType> get_data() {
+			return data_;
+		}
+	protected:
+		std::shared_ptr<DataType> data_;
+};
+
+template<typename ClassType, typename DataType>
+concept SingleContainerLogic = 
+	std::is_function_v<decltype(ClassType::evaluate)> &&
+	requires(ClassType c, single_container<DataType> data)
+{
+	std::is_base_of_v<data_logic<single_container<DataType>>, decltype(c)>;
+};
+
+template<typename ClassType, typename DataType>
+concept PairedContainerLogic =
+	std::is_function_v<decltype(ClassType::evaluate)> &&
+	requires(ClassType c, paired_container<DataType> data)
+{
+	std::is_base_of_v<data_logic<paired_container<DataType>>, decltype(c)>;
+};
+
+template<typename ClassType, typename DataType>
+concept MultiSampleContainerLogic =
+	std::is_function_v<decltype(ClassType::evaluate)> &&
+	requires(ClassType c, multi_sample_data<DataType> data)
+{
+	std::is_base_of_v<data_logic<multi_sample_data<DataType>>, decltype(c)>;
+};
+
+template<typename ResultType>
+class algorhithm
+{
+	public:
+		ResultType get_last_result() { return last_result_; }
+	protected:
+		ResultType last_result_;
+};
+
+template<typename AlgorhithmType, typename ResultType>
+concept Algorhithm = requires(AlgorhithmType alg)
+{
+	std::is_base_of_v<algorhithm<ResultType>, decltype(alg)>;
+};
+
+template<typename T>
+concept Funktor = requires(T c)
+{
+	{ c() };
+};
+
+}
+
+#endif
